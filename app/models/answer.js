f0a937e5f7aeab3d488e315b7190e46859e41131
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-25 12:56:37 -0700

    add question and answer models

---
+++ app/models/answer.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  date: DS.belongsTo('post', {asyn: true}),
+  answer: DS.belongsTo('question' {asyn: true})
+});
