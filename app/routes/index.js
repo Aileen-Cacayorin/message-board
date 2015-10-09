9bc0733ff6ce6d1320d998a7478b930c723ba0e8
Aileen <student@epicodus.com>
2015-10-09 12:07:55 -0700

    change model hook for index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  // why do i need to do this?  
+  // why do i need to do this?
   beforeModel: function() {
   return this.get("session").fetch().catch(function() {});
   },
   model() {
-    return this.store.findAll('question', 'user')
-  },
+      return Ember.RSVP.hash({
+        users: this.store.findAll('user'),
+        locations: this.store.findAll('question')
+      });
+    },
 
-  actions: {
+    actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
